03ddeb205dc4d05b04ed430e0a4f82bb616a6592
jsdwrld <123333887+JSDWRLD@users.noreply.github.com>
2024-11-10 17:22:12 -0800

    Added logos

--- main.js
+++ main.js
@@ -1,23 +1,13 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
+import logo from '/fulllogo.png'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
     <div class="card">
+      <img src="${logo}" href="LuckyLedger Logo"/>
       <button id="counter" type="button"></button>
     </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
   </div>
 `
 
