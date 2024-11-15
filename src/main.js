36bd1fe0a4c19d0e6333b4740f481ad9d8b5a898
jsdwrld <123333887+JSDWRLD@users.noreply.github.com>
2024-11-14 21:25:31 -0800

    Updated ui added animations

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import '../src/style.css';
-import logo from '/fulllogo.png';
-import { setupCounter } from './counter.js';
 import { setupDropdown } from './dropdown.js';
 
 
@@ -26,9 +24,5 @@ window.addEventListener('scroll', () => {
   }
   lastScrollY = window.scrollY;
 });
-
-
-
-setupCounter(document.querySelector('#counter'));
 
 
